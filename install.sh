516915b3c6c8ac002b6f2bef7a7af5ae7e414370
Micu Matei-Marius <micumatei@gmail.com>
2015-12-30 02:01:46 +0200

    Adaugat aliast in install pentru tmux si nvim

--- install.sh
+++ install.sh
@@ -2,23 +2,23 @@
 
 ./update_upgrade.sh
 
+# neovim
 ./nvim_install/install_neovim.sh
 
+#tmux
 ./nvim_install/install_tmux.sh
-
-./update_upgrade.sh
-
-
 
 ./update_upgrade.sh
 
 echo "Creating the simlynks ... "
 
 ./nvim_instal/link_nvim.sh
-
 ./tmux_instal/link_tmux.sh
 
 echo "Creating simlynks Done!"
 
+echo "Creare de alias!"
+./nvim_install/alias.sh
+./tmux_install/alias.sh
 
 ./help.sh
